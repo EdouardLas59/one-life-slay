18cc7c45007b4f527f0419895e9700236fa111f4
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <edouard.lassus@hotmail.fr>
2024-11-18 17:50:10 +0100

    Install Device and created models movie/booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,52 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_18_164236) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookings", force: :cascade do |t|
+    t.date "date_start"
+    t.date "date_end"
+    t.integer "total_price"
+    t.string "pickup_place"
+    t.bigint "user_id", null: false
+    t.bigint "movie_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["movie_id"], name: "index_bookings_on_movie_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.string "year"
+    t.string "director"
+    t.string "actors"
+    t.string "genre"
+    t.string "review"
+    t.string "quality"
+    t.integer "price"
+    t.float "rating"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_movies_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "bookings", "movies"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "movies", "users"
 end
