4894c38f36eadb7f3c30daaec057f15d61555da6
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <edouard.lassus@hotmail.fr>
2024-11-19 17:59:22 +0100

    HP_day_one

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   resources :movies
 
-
   # Defines the root path route ("/")
   # root "posts#index"
 end
